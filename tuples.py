7196c73f73ce1832186a2bbbbe1b026d4559135b
sanjana <sanjana.25bai10433@vitbhopal.nic.in>
2025-09-13 10:40:18 +0530

    this is basics of tuple

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# Arithmetic operations
+
+result1 = 10 + 30
+result2 = 40 - 10
+result3 = 50 * 5
+result4 = 16 / 4
+result5 = 25 % 2
+result6 = 5 ** 3
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
